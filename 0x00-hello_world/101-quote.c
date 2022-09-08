80f5d572f92f1c95ff6ef2f658f71c83cf4bb6f6
Reuben <emavwoyareuben@gmail.com>
2022-09-08 16:06:44 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 11);
+{write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (0);
 }
