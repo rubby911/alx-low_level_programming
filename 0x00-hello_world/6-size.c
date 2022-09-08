6564c50d3588250d3c1a24577b8ac305a375796c
Reuben <emavwoyareuben@gmail.com>
2022-09-08 10:41:04 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ float type_float;
 char type_char;
 long long type_very_long;
 long type_long;
-printf("Size of a char:%zu byte(s)\n", sizeof(type_char));
+printf("Size of a char: %zu byte(s)\n", sizeof(type_char));
 printf("Size of an int: %zu byte(s)\n", sizeof(type_int));
 printf("Size of a long int: %zu byte(s)\n", sizeof(type_long));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(type_very_long));
