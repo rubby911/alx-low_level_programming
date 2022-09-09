a575fca9d47044f955cb5942197b9ba54d549069
Reuben <emavwoyareuben@gmail.com>
2022-09-09 14:54:13 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{char abc = 'a';
+for (; abc <= 'z'; abc++)
+{putchar(abc);
+}
+putchar('\n');
+return (0);
+}
