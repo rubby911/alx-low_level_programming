d6638f2f95526f47b2f69732a2ec61b209e1ca81
Reuben <emavwoyareuben@gmail.com>
2022-09-09 14:10:42 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ printf("Last digit of %d is %d and is 0\n", n, lastdigit);
 }
 else
 {
-printf("Last digit of %d is %d and is less than 6 and not 0\n",n, lastdigit);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
 }
 return (0);
 }
