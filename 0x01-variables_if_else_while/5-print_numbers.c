9d26d8d4f8a479d173a727813c55a13e4fa31585
Reuben <emavwoyareuben@gmail.com>
2022-09-09 15:33:07 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{int first = 0;
+for (; first < 10; first++)
+{printf("%d", first);
+}
+printf("\n");
+return (0);
+}
