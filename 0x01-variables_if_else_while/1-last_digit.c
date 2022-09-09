05bc282e917afcaab02efb6506b1bff4699356db
rubby911 <emavwoyareuben@gmail.com>
2022-09-09 17:52:19 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ if (lastdigit > 5);
 {printf("Last digit of %d is %d and is greater than 5\n", n, lastdigit);
 }
 else if (lastdigit == 0);
-{printf("Last digit of %d is %d and is 0\n", n, lastdigit)
+{printf("Last digit of %d is %d and is 0\n", n, lastdigit);
 }
 else
 {printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
