7996fa4e09a578facd3edcf9a82996391eddeadb
Reuben <emavwoyareuben@gmail.com>
2022-09-10 16:58:24 -0700

    TASK 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,9 +11,9 @@ int main(void)
 {int n1;
 int m1;
 for (n1 = 48; n1 <= 56; n1++)
-
 {for (m1 = 49; m1 <= 57; m1++)
-{if (m1 > n1)
+{
+if (m1 > n1)
 {putchar(n1);
 putchar(m1);
 if (n1 != 56 || m1 != 57)
