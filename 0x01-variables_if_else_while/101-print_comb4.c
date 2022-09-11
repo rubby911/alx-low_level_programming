21b6a330f8afcab32aaa1247a309707f5aa65155
Reuben <emavwoyareuben@gmail.com>
2022-09-10 17:36:12 -0700

    TASK 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - program that prints the numbers from 00 to 99
+* Numbers must be separated by ,followed by a space
+* Numbers should be printed in ascending order with 2 digits
+* You can only use the putchar function
+* Return: 0
+*/
+
+
+int main(void)
+{int first;
+int second;
+int third;
+for(first = 48; first < 58; first++)
+{for (second = 49; second < 58; second++)
+{for (third = 50; third < 58; third++)
+if (third > second && second > first)
+{putchar(first);
+putchar(second);
+putchar(third);
+if (first != 55 || second != 56)
+
