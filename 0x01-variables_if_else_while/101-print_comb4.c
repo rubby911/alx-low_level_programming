50d1ea4680df9a346587583dfad258679d134fb7
Reuben <emavwoyareuben@gmail.com>
2022-09-10 17:47:02 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,7 +13,7 @@ int main(void)
 {int first;
 int second;
 int third;
-for(first = 48; first < 58; first++)
+for (first = 48; first < 58; first++)
 {for (second = 49; second < 58; second++)
 {for (third = 50; third < 58; third++)
 if (third > second && second > first)
@@ -21,4 +21,12 @@ if (third > second && second > first)
 putchar(second);
 putchar(third);
 if (first != 55 || second != 56)
-
+{putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
