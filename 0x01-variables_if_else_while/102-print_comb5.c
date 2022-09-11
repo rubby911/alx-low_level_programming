c73ae47c487f6838b41bef467fbbf18ea0bedc97
Reuben <emavwoyareuben@gmail.com>
2022-09-10 18:15:54 -0700

    TASK 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+* main - prints all possible combinations of two two-digit numbers
+* Return: Always 0 (Success)
+*/
+
+
+int main(void)
+{int first, second;
+for (first = 0; first < 100; first++)
+{
+for (second = 0; second < 100; second++)
+{if (first < second)
+{putchar((first / 10) + 48);
+putchar((first % 10) + 48);
+putchar(' ');
+putchar((second / 10) + 48);
+putchar((second % 10) + 48);
+if (first != 98 || second != 99)
+{putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
