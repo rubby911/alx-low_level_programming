dd02e2b13c59fb6b6fb78a3511ce67fe6e3e9af5
Reuben <emavwoyareuben@gmail.com>
2022-09-13 04:42:53 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*main - check the code
+*
+*Return: Always 0.
+*/
+
+int main(void)
+{char mine;
+mine = _puthar(_putchar);
+putchar('\n');
+return (0);
+}
