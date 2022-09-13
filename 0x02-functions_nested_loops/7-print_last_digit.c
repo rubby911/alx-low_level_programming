af753c2d8aad96f936e2d332cdc994dd37887683
Reuben <emavwoyareuben@gmail.com>
2022-09-13 15:00:49 -0700

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*print_last_digit -  prints the last digit  of an interger number.
+*@a: The interger  size to be checked
+*Return: eiter 0 or 1
+*/
+
+int print_last_digit(int a)
+{
+if ((a % 10) < 0)
+{a *= -1;
+_putchar((a % 10) + '0');
+return (a % 10);
+}
+}
