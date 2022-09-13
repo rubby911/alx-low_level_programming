764222daab849cec7b7d7d8cadc4715c4effaa77
Reuben <emavwoyareuben@gmail.com>
2022-09-13 14:39:54 -0700

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,16 +8,16 @@
 
 int print_sign(int n)
 {
-if (n < 0)
-{_putchar(43);
+if (n > 0)
+{_putchar('+');
 return (1);
 }
-else if (n == 0)
-{_putchar(48);
-return (0);
+else if (n < 0)
+{_putchar('-');
+return (-1);
 }
 else
-{_putchar(45);
-return (-1);
+{_putchar('0');
+return (0);
 }
 }
