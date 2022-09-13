b25998011bf201292a7d24cf7a7f18c75b21b0cf
Reuben <emavwoyareuben@gmail.com>
2022-09-13 14:49:44 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*_abs -  prints the sign of a number.
+*@n: The interger  size to be checked
+*Return: eiter 0 or 1
+*/
+
+int _abs(int n);
+{int value;
+if (n < 0)
+{
+return (n * -1);
+}
+else
+{return (n);
+}
+}
+
