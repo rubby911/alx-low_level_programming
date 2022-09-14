0706f10da9a0667cd727ea1b1adc1edb1fdd9933
Reuben <emavwoyareuben@gmail.com>
2022-09-14 16:11:47 -0700

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+#include "main.c"
+/**
+*tester: Test if a given arguement, is either 0, positive, or a negative number.
+*
+*Return:
+*/
+
+int tester()
+{printf(positive_or_negative(0));
+
+return (0);
+}
