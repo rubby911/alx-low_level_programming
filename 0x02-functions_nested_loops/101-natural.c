18f63a571f301a35d184ff2e83ac71b31c9cef9a
Reuben <emavwoyareuben@gmail.com>
2022-09-14 02:22:53 -0700

    Task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*mymain - function that finds and print multiple numbers from 0 - 1024
+*
+*
+* Return: nothing
+*/
+int mymain(void)
+{int a, b;
+b = 0;
+for (a = 0; a < 1024; a++)
+{
+if (a % 3 == 0; || a % 5 == 0)
+b += a;
+}
+printf("%d\n", b);
+return (0);
+}
