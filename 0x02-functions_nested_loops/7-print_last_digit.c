b6574027e4ff8a0f931cd77692ba593b2c932af9
Reuben <emavwoyareuben@gmail.com>
2022-09-14 01:41:53 -0700

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,9 +10,8 @@ int print_last_digit(int a)
 {int last_digit;
 last_digit = a % 10;
 if (last_digit < 0)
-{last_digit *= -1;
+{last_digit = last_digit * (-1);
+}
 _putchar(last_digit + '0');
 return (last_digit);
-}
-return (0);
 }
