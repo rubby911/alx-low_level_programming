7bd51ae34013e63cb03981b5309ab186f5640aed
Reuben <emavwoyareuben@gmail.com>
2022-09-15 08:33:23 -0700

    Debuging task 1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*main - causes an infinite loop
+*Return: 0
+*/
+
+int main(void)
+{i = 0;
+/*while (i < 10)
+*{putchar(i);
+}
+*/
+printf("Infinite loop avoided\\o/\n");
