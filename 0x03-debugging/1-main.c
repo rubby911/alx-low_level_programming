c28849c59b75dce8fc4d4dd49ecde395247cb61b
Reuben <emavwoyareuben@gmail.com>
2022-09-15 08:52:29 -0700

    Debuging task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,10 +6,13 @@
 */
 
 int main(void)
-{i = 0;
+{int i;
+printf("Infinite loop incoming :(\n");
+i = 0;
 /*while (i < 10)
 *{putchar(i);
 }
 */
-printf("Infinite loop incoming :(\n");
-printf("Infinite loop avoided\\o/\n");
+printf("Infinite loop avoided! \\o/\n");
+return (0);
+}
