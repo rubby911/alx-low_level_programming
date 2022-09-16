5255ec46f16b2b40a0ba565ce166886169fc5614
Reuben <emavwoyareuben@gmail.com>
2022-09-15 18:33:44 -0700

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*print_line - prints a straight line on the terminal
+*
+*@n: that's the number of times we print the _
+*
+*Return: void
+*/
+
+
+void print_line(int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+if (n > 0)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
+}
