8a92f43194298b73cd1d23801b21c76a8034503e
Reuben <emavwoyareuben@gmail.com>
2022-09-15 17:31:35 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*mul -Multiplies 2 intergers
+*@a: that's the first one
+*@b: that's the other
+*Return: the product of a and b
+*/
+
+int mul(int a, int b)
+{
+int result;
+
+product = (a * b);
+return (result);
+}
