2db4d3ea507ec99112942a0f153c190ce9889a4b
Reuben <emavwoyareuben@gmail.com>
2022-09-17 13:08:15 -0700

    Task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -16,16 +16,18 @@ for (j1 = 1; j1 <= size; j1++)
 {
 for ((k2 = size - j1); k2 > 0; k2--)
 {
-_putchar(' ')
+_putchar(' ');
+}
 for (k2 = 0; k2 < j1; k2++)
 {
 _putchar('#');
-for (k2 = 0; k2 > j1; k2++)
+}
 if (j1 == size)
 {
-continue;
+continue
+}
 _putchar('\n');
 }
 }
-_putchar('\n');
+_putchar('\n);
 }
