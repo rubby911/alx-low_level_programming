878926a0e8ecdd006247e86ce7a5dba03a7e8cc4
Reuben <emavwoyareuben@gmail.com>
2022-09-19 08:26:31 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*swap_int - Swaps the value of one interger variable to the other
+*@a: Among the values to be swab
+*@b: Still another value
+*return: void
+*/
+
+void swap_int(int *a, int *b)
+{int swapper;
+swapper = *a;
+*a = *b;
+*b = swapper;
+return (0);
+}
+
+
