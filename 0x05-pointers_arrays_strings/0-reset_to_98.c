1f729688873c5887166b4cc4eaad0333facebba3
Reuben <emavwoyareuben@gmail.com>
2022-09-19 08:09:41 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+*reset_to_98 - Modify the value of an int pointer variable to 98
+*@a: the value to be modified
+*Return: Not voidd
+*/
+
+void reset_to_98(int *n)
+{*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Array and strings read me file
