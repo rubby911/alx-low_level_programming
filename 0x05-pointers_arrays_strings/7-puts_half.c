0057ac2d1b68df816c425a88f859055d2e96c04d
Reuben <emavwoyareuben@gmail.com>
2022-09-19 16:08:50 -0700

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,9 +13,9 @@ j = 0;
 for (len = 0; str[len] != '\0'; len++)
 {j++;
 i = (j / 2);
-if ((j % 2) == 1);
+if ((j % 2) == 1)
 {
-len = ((j + 1) / 2);
+len = ((j + 1) / 2)
 }
 }
 for (len = i; str[len] != '\0'; len++)
