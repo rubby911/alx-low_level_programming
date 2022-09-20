bb1dbe5f2e110967a24dc713bc10a798df4fb13e
Reuben <emavwoyareuben@gmail.com>
2022-09-19 17:00:22 -0700

    Task 9

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,4 +9,6 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 void puts2(char *str);
 void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 #endif /* MAIN_H */
