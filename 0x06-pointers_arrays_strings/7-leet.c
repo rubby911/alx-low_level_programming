8c8d17fc11b07fad229e784e96d53956b8e40ed4
Reuben <emavwoyareuben@gmail.com>
2022-09-21 19:15:17 -0700

    Task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,16 +12,13 @@ char *leet(char *values)
 int j;
 char first[] = "aeotlAEOTL";
 char second[] = "4307143071";
-for (i = 0; *(values + 1); i++)
+for (i = 0; values[i] != '\0'; i++)
 {
-for (j = 0; j <= 9; j++)
+for (j = 0; first[j] != '\0'; j++)
 {
-if (first[j] == *(values + i))
-{
-*(values + i) = second[j];
-}
+if (values[i] == first[j])
+values[i] = second[j];
 }
 }
 return (values);
 }
-
