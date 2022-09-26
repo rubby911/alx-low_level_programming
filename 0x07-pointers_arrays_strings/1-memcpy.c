a268936a4cd54a079d2180523720dc7352615717
Reuben <emavwoyareuben@gmail.com>
2022-09-26 13:56:56 -0700

    Task 1 amd 2

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+**_strchr - A function that locates a character in a string.
+*@s: A string arguement
+*@c: The character to find
+*Return: Return 0
+*/
+
+char *_strchr(char *s, char c)
+{int j = 0;
+for (; s[j] >= '\n'; j++)
+{
+if (s[j] == c)
+{
+return (&s[j]);
+}
+}
+return (0);
+}
+
