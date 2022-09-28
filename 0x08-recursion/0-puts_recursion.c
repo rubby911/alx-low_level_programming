121f180f644741c565b2bedde1caf9e41f7a7335
Reuben <emavwoyareuben@gmail.com>
2022-09-28 13:32:40 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,11 +8,12 @@ recursively
 void _puts_recursion(char *s)
 {if (*s != '\0')
 {
+_putchar('\n');
+}
+else
+{
 _putchar(*s);
-_puts_recursion(s + 1);
-}
-}
-else if (*s == '\0')
-{_putchar('\n');
+s++;
+_puts_recursion(s);
 }
 }
