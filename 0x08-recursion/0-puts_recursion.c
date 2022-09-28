f07e7b0b6e511e7c495b91495cc34eab382bd243
Reuben <emavwoyareuben@gmail.com>
2022-09-28 13:47:30 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-*_puts_recursion -A function that prints a given string
-recursively
+*_puts_recursion -A function that prints a given string recursively
 *@s: The string to print
 */
 void _puts_recursion(char *s)
-{if (*s == '\0')
+{
+if (*s == '\0')
 {
 _putchar('\n');
 }
