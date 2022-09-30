bd09fe6247b90533534402c65b3e462191ce3af0
Reuben <emavwoyareuben@gmail.com>
2022-09-30 13:59:13 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*main - Staring point of the programm
+*@argc: The arguement count value
+*@argv: A char array with the size of argc
+*Return: Returns 0
+*/
+
+
+
+
+
+
+int main(int argc __attribute__((unused)), char *argv[])
+{printf("%s\n", argv[0]);
+return (0);
+}
