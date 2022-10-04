c20614f50f10f53eef98ac08fdc123e15b72c9a5
Reuben <emavwoyareuben@gmail.com>
2022-10-04 16:56:56 -0700

    Task 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -39,10 +39,12 @@ return (num);
  */
 void free_everything(char **string, int i)
 {
-for (; i > 0;)free(string[--i]);
+for (; i > 0;)
+{
+free(string[--i]);
 free(string);
 }
-
+}
 
 /**
  * strtow - function that splits string into words
@@ -53,17 +55,25 @@ char **strtow(char *str)
 {int total_words = 0, b = 0, c = 0, length = 0;
 char **words, *found_word;
 if (str == 0 || *str == 0)
+{
 return (NULL);
+}
 total_words = number(str);
 if (total_words == 0)
+{
 return (NULL);
+}
 words = malloc((total_words + 1) * sizeof(char *));
 if (words == 0)
+{
 return (NULL);
+}
 for (; *str != '\0' &&  b < total_words;)
 {
 if (*str == ' ')
+{
 str++;
+}
 else
 {
 found_word = str;
@@ -74,6 +84,7 @@ str++;
 }
 words[b] = malloc((length + 1) * sizeof(char));
 if (words[b] == 0)
+{
 free_everything(words, b);
 return (NULL);
 }
